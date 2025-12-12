6f39feca18d5a6864e06abfc4fb26bee0f930e1e
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-12-12 06:05:48 +0530

    **__^

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myprojectdb',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'myproject',
         'USER' : 'myprojectuser',
-        'PASSWORD' : '******',
+        'PASSWORD' : 'Alpha&007',
         'HOST' : 'localhost',
         'PORT' : '',
     }
---
+++ myprojectdb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyprojectdbConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myprojectdb'
